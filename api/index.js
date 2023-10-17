e90ffe924f85d3525ab798f8837abffea28ca236
varun__upadhyay <varun.802.vu@gmail.com>
2023-10-17 19:25:14 +0530

    first register with schema succesfull

--- api/index.js
+++ api/index.js
@@ -1,23 +1,37 @@
-const express = require('express')
-const cors = require('cors')
-// const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
+const bcrypt = require('bcryptjs');
+const bcryptSalt = bcrypt.genSaltSync(10);
+const User = require('./models/Users.js');
+
 app.use(express.json());
-// require('dotenv').config()
+require('dotenv').config();
 
- app.use(cors({
-         credentials: true,
-         origin: 'http://localhost:5173',
-   }) )
+app.use(
+    cors({
+        credentials: true,
+        origin: 'http://localhost:5173',
+    })
+);
 
-// console.log(process.env.MONGO_URL)
-// mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL);
+
+
+
 app.get('/test', (req, res) => {
-    res.json('test ok')
-})
-app.post('/register', (req, res) => {
-    const { name, email, password } = req.body;
-    res.json({ name, email, password });
-})
+    res.json('test ok');
+});
 
-app.listen(4000)
+app.post('/register', async (req, res) => {
+    const { name, email, password } = req.body;
+    const userDoc = await User.create({
+        name,
+        email,
+        password: bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
+});
+
+app.listen(4000);
